94721b2cb2d5e25999fbd0418660b70037229299
Bayo <Bayo.Ade@theaa.com>
2023-08-02 10:40:59 +0100

    Removing bugs from calculator08buggy

---
+++ Writing_a_program_chapter/calculator08buggy.cpp
@@ -0,0 +1,304 @@
+
+/*
+	calculator08buggy.cpp
+
+	Helpful comments removed.
+
+	We have inserted 3 bugs that the compiler will catch and 3 that it won't.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+void error (std::string s)
+{
+  throw std::runtime_error (s);
+}
+
+struct Token {
+	char kind;
+	double value;
+	std::string name;
+	Token(char ch) :kind(ch), value(0) { } // Token for operators - ch = operator
+	Token(char ch, double val) :kind(ch), value(val) { } // Token for numbers - ch = 8, value = floating point number
+    Token(char ch, std::string n) :kind(ch), name(n) {} // Token for variables ch = name (constant), name = variable name
+};
+
+class Token_stream {
+	bool full;
+	Token buffer;
+public:
+	Token_stream() :full(0), buffer(0) { }
+	Token get();
+	void unget(Token t) { buffer = t; full = true; }
+	void ignore(char);
+};
+
+const char let = 'L';
+const char quit = 'q';
+const char print = ';';
+const char number = '8';
+const char name = 'a';
+const std::string declkey = "let";
+const int k = 1000;
+
+
+Token Token_stream::get()
+{
+	if (full) { full = false; return buffer; }
+	char ch;
+	std::cin >> ch;
+	switch (ch) {
+    case quit:
+        exit(0);
+        break;
+	case '(':
+	case ')':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case ';':
+	case '=':
+		return Token(ch); // Return Token for operator
+	case '.':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	{	std::cin.unget();
+	double val;
+	std::cin >> val;
+	return Token(number, val); // Return Token for floating point number
+	}
+	default:
+		if (isalpha(ch)) {
+			std::string s;
+			s += ch;
+			while (std::cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+			std::cin.unget();
+			if (s == declkey) return Token(let);
+			return Token(name, s);
+		}
+		error("Bad token");
+	}
+}
+
+void Token_stream::ignore(char c)
+{
+	if (full && c == buffer.kind) {
+		full = false;
+		return;
+	}
+	full = false;
+
+	char ch;
+	while (std::cin >> ch)
+		if (ch == c) return;
+}
+
+struct Variable {
+	std::string name;
+	double value;
+	Variable(std::string n, double v) :name(n), value(v) { }
+};
+
+std::vector<Variable> names;
+
+double get_value(std::string s)
+{
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return names[i].value;
+    std::string err_msg = "get: undefined name " + s;
+	error(err_msg);
+}
+
+void set_value(std::string s, double d)
+{
+	for (int i = 0; i <= names.size(); ++i)
+		if (names[i].name == s) {
+			names[i].value = d;
+			return;
+		}
+    std::string err_msg = "set: undefined name " + s;
+	error(err_msg);
+}
+
+bool is_declared(std::string s)
+{
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return true;
+	return false;
+}
+
+Token_stream ts;
+
+double expression();
+
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case '(':
+	{	double d = expression();
+	    t = ts.get();
+	    if (t.kind != ')') error("')' expected");
+        return d;
+	}
+	case '-':
+		return - primary();
+	case number:
+		return t.value;
+	case name:
+    {
+        Token t2 = ts.get();
+        if (t2.kind == '=') {
+            double d = expression();
+            set_value(t.name, d);
+            return get_value(t.name);
+        }
+        ts.unget(t2);
+        return get_value(t.name);
+    }
+	default:
+		error("primary expected");
+	}
+}
+
+double term()
+{
+	double left = primary();
+	while (true) {
+		Token t = ts.get();
+		switch (t.kind) {
+		case '*':
+			left *= primary();
+			break;
+		case '/':
+		{	double d = primary();
+            if (d == 0) error("divide by zero");
+            left /= d;
+            break;
+		}
+		default:
+			ts.unget(t);
+			return left;
+		}
+	}
+}
+
+double expression()
+{
+	double left = term();
+	while (true) {
+		Token t = ts.get();
+		switch (t.kind) {
+		case '+':
+			left += term();
+			break;
+		case '-':
+			left -= term();
+			break;
+		default:
+			ts.unget(t);
+			return left;
+		}
+	}
+}
+
+// declaring a variable
+double declaration()
+{
+	Token t = ts.get();
+	if (t.kind != 'a') error("name expected in declaration");
+	std::string name = t.name;
+    std::string err_msg = name + " declared twice";
+	if (is_declared(name)) error(err_msg);
+	Token t2 = ts.get();
+	if (t2.kind != '='){
+        err_msg = "= missing in declaration of " + name;
+        error(err_msg);
+    }
+	double d = expression();
+	names.push_back(Variable(name, d));
+	return d;
+}
+
+
+double assignment(std::string name)
+{
+    double d = expression();
+    set_value(name, d);
+    return d;
+}
+
+// statement - either declaration
+double statement()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.unget(t);
+		return expression();
+	}
+}
+
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
+
+const std::string prompt = "> ";
+const std::string result = "= ";
+
+void calculate()
+{
+	while (std::cin) {
+        try {
+            std::cout << prompt;
+            Token t = ts.get();
+            while (t.kind == print) t = ts.get();
+            if (t.kind == quit) return;
+            ts.unget(t);
+            std::cout << result << statement() << '\n';
+        }
+        catch (std::runtime_error& e) {
+            std::cerr << e.what() << '\n';
+            clean_up_mess();
+        }
+    }
+}
+
+int main()
+{
+
+    try {
+        calculate();
+        return 0;
+    }
+    catch (std::exception& e) {
+        std::cerr << "exception: " << e.what() << '\n';
+        char c;
+        while (std::cin >> c && c != ';');
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "exception\n";
+        char c;
+        while (std::cin >> c && c != ';');
+        return 2;
+    }
+
+}
